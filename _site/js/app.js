d63b8bbf63131021b9e60342fb0ab965c264face
Jim Dabrowski <jimdab@mac.com>
2018-10-06 18:44:51 -0500

    Kitchen is done

---
+++ _site/js/app.js
@@ -0,0 +1,10 @@
+$(window).load(function() {
+    $('.flexslider').flexslider({
+    animation: "fade",
+    controlNav: false,
+    directionNav: true,
+    slideshowSpeed: 3500,
+    animationSpeed: 750,
+    touch: true
+    });
+});
